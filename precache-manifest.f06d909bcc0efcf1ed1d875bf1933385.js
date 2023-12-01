f130c696381619508257384771a597316713c289
flameuss <luis.henrique_campos@outlook.com.br>
2023-12-01 19:37:51 -0300

    Updates

---
+++ precache-manifest.f06d909bcc0efcf1ed1d875bf1933385.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b35169a493a3b79c5c67619d1b7dc5b",
+    "url": "/publishdata-luccmebr/index.html"
+  },
+  {
+    "revision": "ec08c4c271e32124dc19",
+    "url": "/publishdata-luccmebr/static/css/main.6329b3a2.chunk.css"
+  },
+  {
+    "revision": "26d027584f370c84b7d3",
+    "url": "/publishdata-luccmebr/static/js/2.103711c9.chunk.js"
+  },
+  {
+    "revision": "6211132799115a7b047d842c9563abc0",
+    "url": "/publishdata-luccmebr/static/js/2.103711c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec08c4c271e32124dc19",
+    "url": "/publishdata-luccmebr/static/js/main.c9fe75fd.chunk.js"
+  },
+  {
+    "revision": "8b2e7c9d0a86877f17de",
+    "url": "/publishdata-luccmebr/static/js/runtime-main.5ed9715a.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/publishdata-luccmebr/static/media/getFetch.013f0b90.cjs"
+  }
+]);
